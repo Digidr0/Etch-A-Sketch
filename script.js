191ea36b06dddd5513a765a767ed556d9f1033e9
Digdiro <ildar.grankin2@gmail.com>
2022-03-16 12:56:08 +0300

    add toggle: hover <=> click

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ const btnNew = document.querySelector(".button-new");
 const btnClear = document.querySelector(".button-clear");
 const paintColorInput  = document.querySelector('#paint-color');
 const baseColorInput  = document.querySelector('#base-color');
-const toggleInput = document.querySelector('.toggle');
 let paintColor = 'crimson'; //none
-
 let defaultColor = 'white';
+
+let inputType = "click"
 
 let gridBlock = {};
 let cell = [];
 let z = 16
 
 const gridSize = 640; //px
+
+function toggle(){
+  var toggleInput = document.getElementById("toggle-mouse-click");
+    gridBlock.forEach(e =>{
+  if (toggleInput.checked == true){
+    e.removeEventListener("click", activatePen);
+    e.addEventListener("mouseover", activatePen);
+  } else {
+    e.removeEventListener("mouseover", activatePen);
+    e.addEventListener("click", activatePen);
+  }
+})
+}
 
 function createGrid(z) {
   for (c = 0; c < (z*z); c++) {
@@ -22,7 +35,9 @@ function createGrid(z) {
     // cell.innerText = (c + 1);
     gridContainer.appendChild(cell).className = "grid-block";
     cell.appendChild;
-    cell.addEventListener('mouseover', activatePen);
+    cell.addEventListener(inputType, activatePen);
+    console.log(inputType);
+
   };
   gridBlock = document.querySelectorAll(".grid-block");
 };
@@ -67,8 +82,4 @@ paintColorInput.addEventListener("change", colorUpdate)
 baseColorInput.addEventListener("change", colorFill)
 paintColorInput.addEventListener("mouseover", colorUpdate)
 
-toggleInput.addEventListener("mouseover", function(e){
- if (e.cheked){
-   console.log(true);
- } else console.log(e);
-})
+
