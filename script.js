a15c019bacc7d76b147ede6b38188b68bc87c22f
Digdiro <ildar.grankin2@gmail.com>
2022-03-17 12:54:03 +0300

    formatting

--- script.js
+++ script.js
@@ -1,104 +1,116 @@
 const gridContainer = document.querySelector(".grid-container");
-const btnNew = document.querySelector('.button-new');
-const btnClear = document.querySelector('.button-clear');
-const paintColorInput  = document.querySelector('.paint-color');
-const baseColorInput  = document.querySelector('.base-color');
-const rainbowColorInput = document.querySelector('#rainbow-checkbox');
-const gridSizeInput = document.querySelector('.grid-size');
-const sizeTitle = document.querySelector('.size-title');
-const toggleTextLeft = document.querySelector('.toggle-text-left');
-const toggleTextRight = document.querySelector('.toggle-text-right');
+const btnNew = document.querySelector(".button-new");
+const btnClear = document.querySelector(".button-clear");
+const paintColorInput = document.querySelector(".paint-color");
+const baseColorInput = document.querySelector(".base-color");
+const rainbowColorInput = document.querySelector("#rainbow-checkbox");
+const gridSizeInput = document.querySelector(".grid-size");
+const sizeTitle = document.querySelector(".size-title");
+const toggleTextLeft = document.querySelector(".toggle-text-left");
+const toggleTextRight = document.querySelector(".toggle-text-right");
 
-let paintColor = 'crimson';
-let defaultColor = 'white';
-let InputType = "click"
+let paintColor = "crimson";
+let defaultColor = "white";
+let InputType = "click";
 
 let gridBlock = {};
 let cell = [];
-let z = 10
+let z = 10;
 
-const gridSize = screen.height*0.9; //px
+function gridSize() {
+  if (screen.width > screen.height) {
+    return screen.height * 0.75;
+  } else {
+    return screen.width * 0.75;
+  }
+}
 
 function randomColor() {
-  let h = Math.floor(Math.random() * 255);
-  let s = Math.floor(Math.random() * 40 + 60);
-  let l = Math.floor(Math.random() * 40 + 50);
-  return `hsl(${h}, ${s}%, ${l}%)`
+  let h = Math.floor(Math.random() * 360);
+  let s = Math.floor(Math.random() * 30 + 60);
+  let l = Math.floor(Math.random() * 30 + 50);
+  return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
 function createGrid(z) {
-  for (c = 0; c < (z*z); c++) {
-    gridContainer.style.setProperty('--grid-z', z);
-    gridContainer.style.setProperty('--cell-size', `${gridSize/z}px`);
+  for (c = 0; c < z * z; c++) {
+    gridContainer.style.setProperty("--grid-z", z);
+    gridContainer.style.setProperty("--cell-size", `${gridSize() / z}px`);
     let cell = document.createElement("div");
 
     gridContainer.appendChild(cell).className = "grid-block";
     cell.appendChild;
     cell.addEventListener(InputType, activatePen);
-  };
+  }
   gridBlock = document.querySelectorAll(".grid-block");
-};
-
-function clearGrid() {
-    gridBlock.forEach(e => {
-    e.style = `background-color: var()`;
-    });
 }
 
-function colorFill() {
-  gridBlock.forEach(e => {
-  e.style = `background-color: ${this.value}`;
+function clearGrid() {
+  gridBlock.forEach((e) => {
+    e.style = `background-color: var()`;
   });
 }
 
+function colorFill() {
+  gridBlock.forEach((e) => {
+    e.style = `background-color: ${this.value}`;
+  });
+}
 
 function activatePen(e) {
-  if (rainbowColorInput.checked == false){
-  this.style = `background-color: ${paintColor}`;
+  if (rainbowColorInput.checked == false) {
+    this.style = `background-color: ${paintColor}`;
   } else {
-  this.style = `background-color: ${randomColor()}`;
+    this.style = `background-color: ${randomColor()}`;
+    document.documentElement.style.setProperty(
+      `--rainbow-color`,
+      randomColor()
+    );
   }
 }
 
-function colorUpdate(){
+function colorUpdate() {
   paintColor = this.value;
-  document.documentElement.style.setProperty(`--second-color`, this.value)
+  document.documentElement.style.setProperty(`--second-color`, this.value);
 }
 
-function toggle(){
+function toggle() {
   let toggleInput = document.querySelector("#toggle");
-    gridBlock.forEach(e =>{
-  if (toggleInput.checked == true){
-    e.removeEventListener("click", activatePen);
-    e.addEventListener("mouseover", activatePen);
-    toggleTextLeft.style.setProperty("color", 'white');
-    toggleTextRight.style.setProperty("color", 'var(--second-color)');
-    InputType = "mouseover";
-  } else {
-    e.removeEventListener("mouseover", activatePen);
-    e.addEventListener("click", activatePen);
-    toggleTextRight.style.setProperty("color", 'white');
-    toggleTextLeft.style.setProperty("color", 'var(--second-color)');
-    InputType = "click";
-  }
-})
+  gridBlock.forEach((e) => {
+    if (toggleInput.checked == true) {
+      e.removeEventListener("click", activatePen);
+      e.addEventListener("mouseover", activatePen);
+      toggleTextLeft.style.setProperty("color", "white");
+      toggleTextRight.style.setProperty("color", "var(--second-color)");
+      InputType = "mouseover";
+    } else {
+      e.removeEventListener("mouseover", activatePen);
+      e.addEventListener("click", activatePen);
+      toggleTextRight.style.setProperty("color", "white");
+      toggleTextLeft.style.setProperty("color", "var(--second-color)");
+      InputType = "click";
+    }
+  });
 }
 
 createGrid(z);
 
-gridSizeInput.addEventListener("mousemove", function newGrid() {
-    gridBlock.forEach(e => {
-      e.remove();
-    });
-    createGrid(this.value);
-    sizeTitle.textContent = `${this.value} X ${this.value}`
+gridSizeInput.addEventListener("mousemove", function () {
+  sizeTitle.textContent = `${this.value} X ${this.value}`;
+});
+
+gridSizeInput.addEventListener("click", function () {
+  gridBlock.forEach((e) => {
+    e.remove();
   });
-  
+  createGrid(this.value);
+});
+
 btnClear.addEventListener("click", clearGrid);
-rainbowColorInput.addEventListener("click",   function(){document.documentElement.style.setProperty(`--rainbow-color`, randomColor())});
+rainbowColorInput.addEventListener("click", function () {
+  document.documentElement.style.setProperty(`--rainbow-color`, randomColor());
+});
 
 paintColorInput.addEventListener("change", colorUpdate);
 baseColorInput.addEventListener("change", colorFill);
 paintColorInput.addEventListener("mouseover", colorUpdate);
-
-
